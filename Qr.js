faaf5fffa8b6389b17cdb789706af158c1f7de91
AKASH <santu>
2023-09-01 21:20:21 +0530

    first commit

---
+++ Qr.js
@@ -0,0 +1,7 @@
+let imgBox=document.getElementById('imgBox')
+let qrImage=document.getElementById('qrImage')
+let qrText=document.getElementById('qrText')
+function GenerateQr(){
+qrImage.src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+qrText.value;
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Qr_Code_Generator
